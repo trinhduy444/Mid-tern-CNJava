9e82708a544d97441f215dff649edb8cb893f64d
trinhduy444 <52000655@student.tdtu.edu.vn>
2023-03-31 22:27:43 +0700

    First

---
+++ src/main/java/com/example/SpringCommerceShop/SpringCommerceShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringCommerceShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommerceShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommerceShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringCommerceShop/SpringCommerceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCommerceShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommerceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

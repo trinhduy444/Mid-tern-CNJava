37929ec91e5172d4443eab37aac50681be8a57ad
trinhduy444 <52000655@student.tdtu.edu.vn>
2023-04-09 14:28:42 +0700

    Last Commit

--- src/main/java/com/example/SpringCommerceShop/Config/SecurityConfig.java
+++ src/main/java/com/example/SpringCommerceShop/Config/SecurityConfig.java
@@ -1,34 +1,25 @@
 package com.example.SpringCommerceShop.Config;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.crypto.bcrypt.*;
-//
-//
-////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurationAdapter;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-////
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 //@Configuration
 //@EnableWebSecurity
-public class SecurityConfig {
-	
-//	@Autowired
-//	
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		// TODO Auto-generated method stub
-//		http.authorizeHttpRequests()
-//		.mvcMatchers("/","/cart").permitAll()
-//		.anyRequest().authenticated().and()
-//		.formLogin().loginPage("/login").permitAll();
-//		
-//	}
-
+public class SecurityConfig  {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/", "/cart", "/static/**").permitAll() // cho phép truy cập vào các trang không yêu cầu đăng nhập
+//                .anyRequest().authenticated() // yêu cầu đăng nhập cho các trang còn lại
+//                .and()
+//            .formLogin()
+//                .loginPage("/login") // sử dụng trang login của riêng bạn
+//                .permitAll() // cho phép truy cập vào trang login mà không yêu cầu đăng nhập
+//                .and()
+//            .logout()
+//                .permitAll(); // cho phép truy cập vào trang logout mà không yêu cầu đăng nhập
+//    }
 }
--- src/main/java/com/example/SpringCommerceShop/Controller/HomePageController.java
+++ src/main/java/com/example/SpringCommerceShop/Controller/HomePageController.java
@@ -4,9 +4,17 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import com.example.SpringCommerceShop.Mapper.AccountMapper;
+import com.example.SpringCommerceShop.Modal.*;
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,14 +25,10 @@ import org.springframework.web.servlet.ModelAndView;
 import com.example.SpringCommerceShop.Config.AES;
 import com.example.SpringCommerceShop.Mapper.OrdersMapper;
 import com.example.SpringCommerceShop.Mapper.ProductMapper;
-import com.example.SpringCommerceShop.Modal.MyPageInfo;
-import com.example.SpringCommerceShop.Modal.Orders;
-import com.example.SpringCommerceShop.Modal.Product;
-import com.example.SpringCommerceShop.Modal.ProductExample;
-import com.example.SpringCommerceShop.Modal.ProductWithBLOBs;
 import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
 import jakarta.websocket.server.PathParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -35,14 +39,68 @@ public class HomePageController {
 	
 	@Autowired
 	OrdersMapper ordersMapper;
-	
-	
+
+	@Autowired
+	AccountMapper accountMapper;
+
+	private String secrectKey = "DuyDepTrai@123";
+
+	private boolean isValidUser(String username, String password){
+
+		String passDecode = AES.encrypt(password,secrectKey);
+
+		if(accountMapper.isValidLogin(username).equals(passDecode)) {
+			return true;
+		}
+		return false;
+		
+	}
 	@GetMapping("/login")
 	public ModelAndView login() {
 		ModelAndView modelAndView = new ModelAndView("login");
 		return modelAndView;
 	}
-	
+
+	@PostMapping("/loginSuccess")
+	public ModelAndView login(@RequestParam("fullname") String fullname,@RequestParam("user_id") String user_id,
+							  @RequestParam("email") String email,@RequestParam("password") String password) {
+		ModelAndView modelAndView = new ModelAndView("login");
+		String passwordEncode = AES.encrypt(password,secrectKey);
+		Account account = new Account(user_id,fullname,email,passwordEncode,2);
+		accountMapper.insert(account);
+		return modelAndView;
+	}
+
+
+
+@PostMapping("/login")
+public ModelAndView loginPost(@RequestParam("email") String email, @RequestParam("password") String password, HttpServletResponse response, RedirectAttributes redirectAttributes){
+
+	// process login
+	if(isValidUser(email,password)){
+		Cookie cookie = new Cookie("loginInfo", email);
+		Cookie cookiepass = new Cookie("loginInfoPass", password);
+
+		cookie.setMaxAge(60*60*24); // cookie will expire in 1 day
+		cookiepass.setMaxAge(60*60*24); // cookie will expire in 1 day
+
+		response.addCookie(cookie);
+		response.addCookie(cookiepass);
+		return new ModelAndView("redirect:/");
+	}
+	else {
+		redirectAttributes.addFlashAttribute("error", "Invalid username or password");
+		return new ModelAndView("redirect:/login");
+	}
+}
+
+
+	@GetMapping("/register")
+	public ModelAndView register() {
+		ModelAndView modelAndView = new ModelAndView("register");
+
+		return modelAndView;
+	}
 	@GetMapping("/")
 	public ModelAndView index(@RequestParam(name = "page", defaultValue = "1")int pageNumber) {   
 	    int pageSize = 8; // số lượng mục trên mỗi trang
@@ -52,7 +110,6 @@ public class HomePageController {
 	    ModelAndView modelAndView = new ModelAndView("index");    
 	    MyPageInfo myPageInfo = new MyPageInfo(pageNumber, pageSize, totalItems, totalPages); // tạo đối tượng MyPageInfo
 	    modelAndView.addObject("myPageInfo",myPageInfo);
-	    
 	    List<Map<String, Object>> listMapProduct = productMapper.getProductPage(myPageInfo);
 	    modelAndView.addObject("products",listMapProduct);
 
@@ -88,11 +145,20 @@ public class HomePageController {
 	    return modelAndView;
 	}
 	
-	
+	@GetMapping("/dashboard")
+	public ModelAndView dashBoard(){
+
+		return new ModelAndView("dashboard");
+	}
+	@GetMapping("/profile")
+	public ModelAndView proFile(){
+
+		return new ModelAndView("profile");
+	}
 	@GetMapping("/products/{category}")
 	public ModelAndView getProductByCateGory(@PathVariable String category) {
 		ModelAndView modelAndView = new ModelAndView("index");
-		System.out.println("Cate:"+category);
+
 		
 		int cate=0;
 		if(category.equals("food")) {
@@ -103,15 +169,14 @@ public class HomePageController {
 			cate = 2;
 		}
 		
-		System.out.println(cate);
 		List<Map<String, Object>> listProductsCateList = productMapper.findByCategory(cate);
 	    modelAndView.addObject("products",listProductsCateList);
-//		int pageNumber =1;
-//		int pageSize = 8; // số lượng mục trên mỗi trang
-//		int totalItems = productMapper.getCountProduct(); // tổng số lượng mục trong CSDL
-//		int totalPages = (int) Math.ceil((double) totalItems / pageSize); // tính tổng số trang
-//		MyPageInfo myPageInfo = new MyPageInfo(pageNumber, pageSize, totalItems, totalPages); // tạo đối tượng MyPageInfo
-//		modelAndView.addObject("myPageInfo",myPageInfo);
+		int pageNumber =1;
+		int pageSize = 8; // số lượng mục trên mỗi trang
+		int totalItems = productMapper.getCountProduct(); // tổng số lượng mục trong CSDL
+		int totalPages = (int) Math.ceil((double) totalItems / pageSize); // tính tổng số trang
+		MyPageInfo myPageInfo = new MyPageInfo(pageNumber, pageSize, totalItems, totalPages); // tạo đối tượng MyPageInfo
+		modelAndView.addObject("myPageInfo",myPageInfo);
 		
 		return modelAndView;
 	}
